c68c4035ee188a7994bc9883c26094c61d186a80
Paul Nikiforoff <velfor@tut.by>
2023-02-05 19:55:19 +0300

    Add files via upload

--- settings.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-
-const float WINDOW_WIDTH = 600;
-const float WINDOW_HEIGHT = 1000;
-const std::string WINDOW_TITLE = "SFML Lesson2";
-const float FPS = 60.f;
-
